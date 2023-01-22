dde17c8853b89b68850edbf0241cbaddd343203c
Tanawat Pinthongpan <tanawat@magicboxasia.com>
2023-01-22 16:03:03 +0700

    update button active action

--- src/components/PageUiComponent/Title/Title.tsx
+++ src/components/PageUiComponent/Title/Title.tsx
@@ -1,5 +1,7 @@
 import tw, { styled } from 'twin.macro'
 
+import { TEXT_THEME } from 'constants/twTheme'
+
 type TitleProps = {
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl'
   title: string
@@ -11,35 +13,47 @@ type TwTitleSize = Pick<TitleProps, 'size'>
 const TwContainer = styled.div(({ size }: TwTitleSize) => {
   switch (size) {
     case '2xl':
-      return [tw`mb-12`]
+      return tw`mb-12`
     case 'xl':
-      return [tw`mb-10`]
+      return tw`mb-10`
     case 'lg':
-      return [tw`mb-8`]
+      return tw`mb-8`
     case 'md':
-      return [tw`mb-6`]
+      return tw`mb-6`
     case 'sm':
-      return [tw`mb-4`]
+      return tw`mb-4`
     case 'xs':
-      return [tw`mb-3`]
+      return tw`mb-3`
   }
 })
 
-export const Title: React.FC<TitleProps> = ({ description, title, size = 'md' }) => {
+const TwH = TEXT_THEME.HEAD
+const TwH1 = styled.h1(TwH)
+const TwH2 = styled.h2(TwH)
+const TwH3 = styled.h3(TwH)
+const TwH4 = styled.h4(TwH)
+const TwH5 = styled.h5(TwH)
+const TwH6 = styled.h6(TwH)
+
+export const Title: React.FC<TitleProps> = ({
+  description,
+  title,
+  size = 'md',
+}) => {
   const renderTitleSize = () => {
     switch (size) {
       case '2xl':
-        return <h1>{title}</h1>
+        return <TwH1>{title}</TwH1>
       case 'xl':
-        return <h2>{title}</h2>
+        return <TwH2>{title}</TwH2>
       case 'lg':
-        return <h3>{title}</h3>
+        return <TwH3>{title}</TwH3>
       case 'md':
-        return <h4>{title}</h4>
+        return <TwH4>{title}</TwH4>
       case 'sm':
-        return <h5>{title}</h5>
+        return <TwH5>{title}</TwH5>
       case 'xs':
-        return <h6>{title}</h6>
+        return <TwH6>{title}</TwH6>
     }
   }
 
