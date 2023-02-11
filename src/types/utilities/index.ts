15ffd1c7ebb14924894183684ab56dfb2c6d06c6
Tanawat Pinthongpan <tanawat@magicboxasia.com>
2023-02-11 12:11:13 +0700

    feat(types): :sparkles: union type utilities and document for use

--- src/types/utilities/index.ts
+++ src/types/utilities/index.ts
@@ -1,4 +1,5 @@
 export * from './keyOf'
 export * from './merged'
+export * from './union'
 export * from './withRequired'
 export * from './withSpecRequired'
--- src/types/utilities/withRequired.ts
+++ src/types/utilities/withRequired.ts
@@ -1,1 +1,13 @@
+/**
+ * @file types/utilities/withRequired.ts
+ * @description Make some properties required
+ * @example
+ *  type Data = {
+ *    a?: string
+ *    b?: string
+ *    c?: string
+ *  }
+ *
+ *  type DataRequired = WithRequired<Data, 'a' | 'b'> // { a: string; b: string; c?: string }
+ */
 export type WithRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] }
