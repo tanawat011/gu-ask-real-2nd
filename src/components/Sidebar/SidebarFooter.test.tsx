020315c7e18297032c21f64ca175ff94c4f8d15f
Tanawat Pinthongpan <tanawat@magicboxasia.com>
2023-02-11 22:12:31 +0700

    test(components): :white_check_mark: mock package.json

--- src/components/Sidebar/SidebarFooter.test.tsx
+++ src/components/Sidebar/SidebarFooter.test.tsx
@@ -3,6 +3,9 @@ import { renderWithProviders } from 'utils/jest'
 import { SidebarFooter } from './SidebarFooter'
 
 jest.mock('./SidebarFooter', () => jest.requireActual('./SidebarFooter'))
+jest.mock('../../../package.json', () => ({
+  version: '0.1.0',
+}))
 
 describe('<SidebarFooter />', () => {
   test('should render without crashing and match snapshot', () => {
